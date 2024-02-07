ffd7e04f46fc8b1212b2a6e9dd429a87a146b681
PattheeraTee <pattheera.mwny@gmail.com>
2024-02-07 14:33:47 +0700

    เพิ่ม nav

--- app/src/main/java/com/example/hantanjai/ui/other/OtherFragment.kt
+++ app/src/main/java/com/example/hantanjai/ui/other/OtherFragment.kt
@@ -1,4 +1,44 @@
 package com.example.hantanjai.ui.other
 
-class OtherFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.hantanjai.databinding.FragmentOtherBinding
+import com.example.hantanjai.ui.other.OtherViewModel
+
+
+class OtherFragment : Fragment() {
+
+    private var _binding: FragmentOtherBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val otherViewModel =
+            ViewModelProvider(this).get(OtherViewModel::class.java)
+
+        _binding = FragmentOtherBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textOther
+        otherViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/example/hantanjai/ui/other/OtherViewModel.kt
+++ app/src/main/java/com/example/hantanjai/ui/other/OtherViewModel.kt
@@ -1,4 +1,13 @@
 package com.example.hantanjai.ui.other
 
-class OtherViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class OtherViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is other Fragment"
+    }
+    val text: LiveData<String> = _text
 }
